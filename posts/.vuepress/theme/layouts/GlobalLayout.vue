1726525efa1eaa11e90cfb065faa3b1ebe4af73c
Vakme <4bielak.aleksandra@gmail.com>
2019-12-13 22:44:28 +0100

    new theme

---
+++ posts/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="global-layout">
+        <header><h1>Header</h1></header>
+        <component :is="layout"/>
+        <footer><h1>Footer</h1></footer>
+    </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      layout () {
+        if (this.$page.path) {
+          if (this.$frontmatter.layout) {
+            return this.$frontmatter.layout
+          }
+          return 'Layout'
+        }
+        return 'NotFound'
+      }
+    }
+  }
+</script>
---
+++ posts/.vuepress/theme/layouts/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <img :src="$withBase('/404.svg')" alt="404">
+    </div>
+</template>
