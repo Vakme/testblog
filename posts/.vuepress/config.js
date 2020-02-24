a1930af1ef1563dff38b96dfd1af720971ad5322
Vakme <4bielak.aleksandra@gmail.com>
2020-02-24 22:56:10 +0100

    sample markdown post

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -5,17 +5,19 @@ module.exports = {
   markdown: {
     extendMarkdown: md => {
       // use more markdown-it plugins!
-      md.use(require('markdown-it-footnote'))
-    }
+      md.set({ typographer: true });
+      md.use(require('markdown-it-footnote'));
+      md.use(require('markdown-it-anchor'));
+    },
+    anchor: { permalink: true, permalinkBefore: false, permalinkSymbol: '' }
   },
   head: [
     ['link', {rel: 'stylesheet', href: 'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css'}],
     ['link', {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Work+Sans:300,400,700&display=swap&subset=latin-ext'}],
     ['script', {src: 'https://kit.fontawesome.com/08ddb17d14.js', crossorigin: 'anonymous'}]
   ],
-  plugins: [
-    [
-      '@vuepress/blog',
+  plugins: {
+    '@vuepress/blog':
       {
         directories: [
           {
@@ -52,6 +54,9 @@ module.exports = {
           },
         ]
       },
-    ],
-  ],
+    '@vuepress/plugin-medium-zoom':
+      {
+      selector: '.theme-container :not(a) > img',
+    }
+  }
 }
