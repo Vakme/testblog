444873a75e46bb6d4398d77dc051c1171897f43c
Vakme <4bielak.aleksandra@gmail.com>
2020-02-25 00:02:11 +0100

    footer

--- posts/.vuepress/theme/global-components/TheFooter.vue
+++ posts/.vuepress/theme/global-components/TheFooter.vue
@@ -1,11 +1,18 @@
 <template>
-    <footer class="wide">
-        <div class="tags">
-            <span class="tag" v-for="tag of $tag._metaMap" :style="`font-size: ${12 + 2*tag.pages.length}px`">
-                    <router-link class="uk-link-text" :to="tag.path">
-                        #{{tag.key}}
-                    </router-link>
-            </span>
+    <footer>
+        <div class="footer-container">
+            <div class="uk-flex uk-flex-center uk-flex-middle" >
+                <router-link to="/" exact><img class="uk-logo" :src="$withBase('/web-logo-small-white.png')" alt="logo"></router-link>
+            </div>
+            <div class="tags">
+                <span class="tag" v-for="tag of $tag._metaMap" :style="`font-size: ${12 + 2*tag.pages.length}px`">
+                        <router-link class="uk-link-text" :to="tag.path">
+                            #{{tag.key}}
+                        </router-link>
+                </span>
+            </div>
+            <hr class="uk-divider-icon divider">
+            <div class="uk-flex uk-flex-center uk-flex-middle uk-text-small">© Basement Code, 2019. Wszelkie prawa zastrzeżone</div>
         </div>
     </footer>
 </template>
@@ -16,6 +23,32 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    footer {
+        background-color: black;
+        height: 300px;
+        &:before {
+            content: '';
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 300px;
+            background-repeat: no-repeat;
+            opacity: 0.16;
+            background-position: center center;
+            background-size: cover;
+            background-image: url("/background.jpg");
+            z-index: 1;
+         }
+    }
+    .footer-container {
+        padding: 50px 10vw;
+        color: white;
+        z-index: 100;
+        position: absolute;
+        width: 80vw;
+    }
+    .divider {
+        opacity: 0.2;
+    }
 </style>
